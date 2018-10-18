8bc686853b3ffd2b6dba249c617f3f3eb16a080b
Eric Lai <small90214@gmail.com>
2018-10-18 15:56:17 +0800

    first

---
+++ src/pages/Test.vue
@@ -0,0 +1,15 @@
+<template>
+  <q-page class="flex flex-center">
+    TEst Page
+    <q-btn style="background: goldenrod; color: white" label="Goldenrod" />
+  </q-page>
+</template>
+
+<style>
+</style>
+
+<script>
+export default {
+  name: 'PageIndex'
+}
+</script>
